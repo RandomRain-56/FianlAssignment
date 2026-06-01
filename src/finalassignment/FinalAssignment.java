46ecbd2d9f9089f2b0fbc4fcf1befeb643701139
350325485 <350325485@C-5W0XS13.tdsb.on.ca>
2026-06-01 11:02:20 -0400

    First Commit

---
+++ src/finalassignment/FinalAssignment.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package finalassignment;
+/**
+ *
+ * @author 350325485
+ */
+public class FinalAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GraphicDraw.main("finalassignment.GraphicDraw");
+    }
+    
+}
---
+++ src/finalassignment/GraphicDraw.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package finalassignment;
+import processing.core.PApplet;
+
+/**
+ *
+ * @author 350325485
+ */
+public class GraphicDraw extends PApplet{
+    
+}
